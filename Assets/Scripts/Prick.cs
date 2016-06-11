bacdbc77a9a505f61fca2b312a15aa9816807477
Isak Sondell <isak.sondell@gmail.com>
2016-06-11 16:51:01 +0200

    init

---
+++ Assets/Scripts/Prick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prick : MonoBehaviour {
+
+    private MeshCollider m_collider;
+
+    void Start ()
+    {
+        m_collider = GetComponent<MeshCollider>();
+	}
+	
+	void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.tag == "PrickBall")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/TransportMover.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TransportMover : MonoBehaviour
+{
+    private List<GameObject> m_transportPoints;
+    private Rigidbody2D m_rigidbody;
+    private bool m_plotting = true;
+
+    [SerializeField]
+    private float m_plotRate;
+    private float m_plotCooldown;
+    [SerializeField]
+    private float m_forceFactor;
+
+
+    void Start ()
+    {
+        m_transportPoints = new List<GameObject>();
+        m_rigidbody = gameObject.GetComponent<Rigidbody2D>();        
+    }
+	
+
+	void Update ()
+    {
+        PlotCourse();
+        AddMomentum();
+    }
+
+
+    void PlotCourse()
+    {
+        if (m_plotting && m_plotCooldown >= m_plotRate)
+        {
+            m_plotCooldown = 0;
+
+            GameObject point = new GameObject();
+            point.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.x, 0);
+            
+
+            m_transportPoints.Add(point);
+        }
+        m_plotCooldown += Time.deltaTime;
+    }
+
+    void AddMomentum()
+    {
+        if (m_transportPoints.Count > 0)
+        {
+            m_rigidbody.AddForce((gameObject.transform.position - m_transportPoints[0].transform.position) * m_forceFactor * Time.deltaTime);
+        }
+    }
+}
+
+

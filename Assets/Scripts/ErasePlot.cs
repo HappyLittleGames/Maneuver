534c2b9b5c0982908f7ef6f25c8bdaae3923cdb1
Isak Sondell <isak.sondell@gmail.com>
2016-06-11 18:19:29 +0200

    plotting prototype functional

---
+++ Assets/Scripts/ErasePlot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ErasePlot : MonoBehaviour
+{
+
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/TrackMouse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackMouse : MonoBehaviour
+{
+	
+
+	void Update ()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+	}
+}
